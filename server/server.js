2c3052b20118ad3cdf2d53a691d3b55137aa57af
githubmave <lidanmei@yahoo.com>
2020-12-06 09:25:28 +1300

    add:Posts.jsx,Post.jsx,PostForm.jsx

--- server/server.js
+++ server/server.js
@@ -3,6 +3,8 @@ const path = require('path')
 
 const videoRoutes = require('./routes/videos')
 const repos = require('./routes/repos')
+const postRoutes = require('./routes/posts')
+
 
 const server = express()
 
@@ -11,5 +13,7 @@ server.use(express.static(path.join(__dirname, 'public')))
 
 server.use('/api/v1/videos', videoRoutes)
 server.use('/api/v1/repos', repos)
+server.use('/api/v1/posts', postRoutes)
+
 
 module.exports = server
