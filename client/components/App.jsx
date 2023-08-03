22f512eb889c7d6fbf525bfd49577ed7885c1f00
githubmave <lidanmei@yahoo.com>
2023-08-03 23:32:15 +1200

    asfd

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,65 +1,29 @@
 import React from 'react'
-import { connect } from 'react-redux'
-//import Nav from './Nav'
+import { HashRouter as Router, Route } from 'react-router-dom'
 
-import { fetchFruits ,recieveVideos} from '../actions'
-import fetchVideos from '../apis/videos'
+import VideoList from './VideoList'
+import VideoItem from './VideoItem'
+import VideoNames from './VideoNames'
+import Repos from './Repos'
 
-export class App extends React.Component {
-  // state = {
-  //   fruits: []
-  // }
-
-  state = {
-    videos: []
-  }
-
-  componentDidMount () {
-   // this.props.dispatch(fetchFruits())
-
-    fetchVideos()
-    //  console.log(res.body)
-      .then (videoData => {
-        console.log(videoData)
-       // this.setState({videos: res})
-        this.props.dispatch(recieveVideos(videoData))
-        
-      })
-       // console.log("what is in state") 
-      .catch( err => console.log(err))
-      
-
-  }
-
+export default class App extends React.Component {
   render () {
     return (
-      <div className='app'>
-        <h1>Fullstack Boilerplate - with Fruits!</h1>
-        {/* <ul>
-          {this.props.fruits.map(fruit => (
-            <li key={fruit}>{fruit}</li>
-          ))}
-        </ul> */}
-      <div >
-        {/* <Nav /> */}
-      </div>
-
-        <ul>
-
-            {this.props.videos.map( (video,i) => (<li key = {i}> {video.video_name}  </li>))}
-        </ul>
-      </div>
+      <>
+        <Router>
+          <div className="app">
+            <h1>Fullstack with video!</h1>
+            <div>
+              <VideoList />
+              <Route path="/videos/:category" component={VideoNames} />
+              <Route path="/videos/:category/:name" component={VideoItem} />
+            </div>
+            <section>
+              <Repos/>
+            </section>
+          </div>
+        </Router>
+      </>
     )
   }
 }
-
-function mapStateToProps (globalState) {
-  return {
-    //fruits: globalState.fruits,
-    videos: globalState.videos
-  }
-}
-
-//export default App
-
-export default connect(mapStateToProps)(App)
